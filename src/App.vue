28141d5af94f8e3cf757859946eccb47c47427f1
Tassilo Dietze <t.dietze@itdotmedia.de>
2023-10-13 13:50:08 +0200

    Changed focus on items when adding

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 import type { Item } from './types';
-import { PlusCircleIcon, XCircleIcon } from '@heroicons/vue/20/solid';
+import { PlusCircleIcon, TrashIcon } from '@heroicons/vue/20/solid';
 import ItemInputGroup from './components/ItemInputGroup.vue';
 
 const nextItemKey = ref(1);
@@ -9,19 +9,27 @@ const items = ref<Map<number, Item>>(new Map());
 const choosenKey = ref<number>();
 const choosenItem = ref<Item>();
 const startCount = ref(3);
+const focusKey = ref<number>(0);
 
 const totalWeight = computed(() => {
   return [...items.value.values()].reduce((acc, items) => acc + items.weight, 0);
 });
 
-const addItem = (text?: string) => {
-  items.value.set(nextItemKey.value, {
-    text: text ?? `Item ${nextItemKey.value}`,
+const addItem = ({ text, setFocus = false }: { text?: string, setFocus?: boolean } = {}): number => {
+  const itemKey = nextItemKey.value;
+  nextItemKey.value++;
+
+  items.value.set(itemKey, {
+    text: text ?? `Item ${itemKey}`,
     weight: 1,
     count: 0
   });
 
-  nextItemKey.value++;
+  if (setFocus) {
+    focusKey.value = itemKey;
+  }
+
+  return itemKey;
 };
 
 const getItem = (key: number) => {
@@ -39,12 +47,6 @@ const removeItem = (key: number) => {
   if (items.value.size <= 0) {
     nextItemKey.value = 1;
   }
-};
-
-const clearItemCount = (key: number) => {
-  const item = getItem(key);
-
-  item.count = 0;
 };
 
 const chooseRandomItem = () => {
@@ -75,7 +77,9 @@ const getRandomItemKey = (): number | undefined => {
 
 const addItems = () => {
   for (let i = 1; i <= startCount.value; i++) {
-    addItem();
+    addItem({
+      setFocus: (i == 1)
+    });
   }
 };
 
@@ -84,8 +88,15 @@ const removeItems = () => {
   nextItemKey.value = 1;
 };
 
+const itemInputMounted = (key: number, inputField: HTMLInputElement | null) => {
+  if (key == focusKey.value) {
+    inputField?.select();
+    focusKey.value = 0;
+  }
+}
+
 const testChooseItem = (times: number) => {
-  items.value.forEach((item, key) => clearItemCount(key));
+  items.value.forEach((item, key) => item.count = 0);
 
   for (let i = 0; i < times; i++) {
     chooseRandomItem();
@@ -99,47 +110,38 @@ const testChooseItem = (times: number) => {
       <label for="item_count" class="sr-only">Add items</label>
       <div class="flex rounded-md shadow-sm">
         <div class="relative flex items-stretch focus-within:z-10">
-          <input
-            type="number"
-            v-model="startCount"
-            autofocus
+          <input type="number" v-model="startCount" autofocus
             class="block w-full rounded-none rounded-l-md border-0 py-1.5 bg-gray-900 text-gray-100 ring-1 ring-inset ring-gray-800 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
-            @keyup.enter="addItems()"
-          />
+            @keyup.enter="addItems()" />
         </div>
-        <button
-          type="button"
+        <button type="button"
           class="flex-grow relative -ml-px inline-flex items-center justify-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold bg-gray-900 text-primary-600 ring-1 ring-inset ring-gray-800 hover:bg-gray-700"
-          @click="addItems()"
-        >
+          @click="addItems()">
           <PlusCircleIcon class="-ml-0.5 h-5 w-5 text-primary-500" aria-hidden="true" />
           Add Items
         </button>
       </div>
     </div>
-    <button
-      type="button"
-      class="relative inline-flex items-center justify-center gap-x-1.5 rounded-md p-2 text-sm font-semibold bg-gray-900 text-red-600 ring-1 ring-inset ring-gray-800 hover:bg-gray-700"
-      @click="removeItems()"
-    >
-      <XCircleIcon class="h-5 w-5 text-red-500" aria-hidden="true" />
+    <button type="button"
+      class="flex items-center justify-center gap-x-1.5 rounded-md p-2 text-sm font-semibold bg-gray-900 text-red-600 ring-1 ring-inset ring-gray-800 hover:bg-gray-700"
+      @click="removeItems()">
+      <TrashIcon class="h-5 w-5 text-red-500" aria-hidden="true" />
     </button>
   </header>
 
   <main class="p-2">
-    <div>
+    <div class="flex flex-col gap-2 ">
       <ul class="flex flex-col gap-1">
-        <ItemInputGroup
-          v-for="[key, item] in items"
-          :key="key"
-          :index="key"
-          :total-items="items.size"
-          :total-weight="totalWeight"
-          :choosen-key="choosenKey"
-          :item="item"
-          @remove="removeItem"
-        />
+        <ItemInputGroup v-for="[key, item] in items" :key="key" :index="key" :total-items="items.size"
+          :total-weight="totalWeight" :choosen-key="choosenKey" :item="item" @remove="removeItem"
+          @mounted="itemInputMounted" />
       </ul>
+      <button type="button"
+        class="flex w-full items-center justify-center gap-x-1.5 rounded-md p-2 text-sm font-semibold bg-gray-900 text-primary-600 ring-1 ring-inset ring-gray-800 hover:bg-gray-700"
+        @click="addItem({ setFocus: true })">
+        <PlusCircleIcon class="h-5 w-5 text-primary-500" aria-hidden="true" />
+        Add Item
+      </button>
     </div>
     <div class="flex gap-2">
       <button @click="chooseRandomItem()">Get Item</button>
--- src/components/ItemInputGroup.vue
+++ src/components/ItemInputGroup.vue
@@ -18,6 +18,7 @@ const props = defineProps<{
 
 const emits = defineEmits<{
   (e: 'remove', index: number): void;
+  (e: 'mounted', index: number, textField: HTMLInputElement | null): void;
 }>();
 
 const itemTextRef = ref<HTMLInputElement | null>(null);
@@ -39,64 +40,42 @@ const removeItem = () => {
 };
 
 onMounted(() => {
-  itemTextRef.value?.select();
+  emits('mounted', props.index, itemTextRef.value);
 });
 </script>
 <template>
-  <li
-    :class="[
-      index == choosenKey ? 'border-primary-600' : 'border-gray-800',
-      'flex flex-wrap items-center justify-between bg-gray-900 border rounded-md gap-x-6 gap-y-4 px-2 py-5 sm:flex-nowrap'
-    ]"
-  >
+  <li :class="[
+    index == choosenKey ? 'border-primary-600' : 'border-gray-800',
+    'flex flex-wrap items-center justify-between bg-gray-900 border rounded-md gap-x-6 gap-y-4 px-2 py-5 sm:flex-nowrap'
+  ]">
     <div>
       <div>
         <label :for="`item_${index}`" class="sr-only"> Item {{ index }} </label>
         <div class="mt-2 flex rounded-md shadow-sm">
           <span
-            class="inline-flex items-center rounded-l-md border border-r-0 bg-gray-700 border-gray-700 px-3 text-gray-400 sm:text-sm"
-          >
+            class="inline-flex items-center rounded-l-md border border-r-0 bg-gray-700 border-gray-700 px-3 text-gray-400 sm:text-sm">
             {{ index }}
           </span>
-          <input
-            ref="itemTextRef"
-            type="text"
-            v-model="item.text"
-            :name="`item_${index}`"
-            :id="`item_${index}`"
-            autofocus
-            class="block w-full min-w-0 flex-1 rounded-none rounded-r-md border-0 py-1.5 bg-gray-800 text-gray-100 ring-1 ring-inset ring-gray-700 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
-          />
+          <input ref="itemTextRef" type="text" v-model="item.text" :name="`item_${index}`" :id="`item_${index}`" autofocus
+            class="block w-full min-w-0 flex-1 rounded-none rounded-r-md border-0 py-1.5 bg-gray-800 text-gray-100 ring-1 ring-inset ring-gray-700 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6" />
         </div>
       </div>
       <div class="mt-1 flex items-center gap-x-2">
         <div class="flex w-16 gap-x-2.5">
           <div
-            class="inline-flex items-baseline bg-gray-400/10 text-gray-400 rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0"
-          >
-            <TrophyIcon
-              class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-gray-500"
-              aria-hidden="true"
-            />
+            class="inline-flex items-baseline bg-gray-400/10 text-gray-400 rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0">
+            <TrophyIcon class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-gray-500" aria-hidden="true" />
             <span class="sr-only"> Item {{ index }} got choosen </span>
             {{ item.count }}
           </div>
-          <div
-            :class="[
-              itemHasGoodOdds ? 'bg-green-400/10 text-green-400' : 'bg-red-400/10 text-red-400',
-              'inline-flex items-baseline rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0'
-            ]"
-          >
-            <ArrowTrendingUpIcon
-              v-if="itemHasGoodOdds"
-              class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500"
-              aria-hidden="true"
-            />
-            <ArrowTrendingDownIcon
-              v-else
-              class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500"
-              aria-hidden="true"
-            />
+          <div :class="[
+            itemHasGoodOdds ? 'bg-green-400/10 text-green-400' : 'bg-red-400/10 text-red-400',
+            'inline-flex items-baseline rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0'
+          ]">
+            <ArrowTrendingUpIcon v-if="itemHasGoodOdds"
+              class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500" aria-hidden="true" />
+            <ArrowTrendingDownIcon v-else class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500"
+              aria-hidden="true" />
             <span class="sr-only"> Item {{ index }} odds are </span>
             {{ itemChance }}
           </div>
@@ -107,14 +86,10 @@ onMounted(() => {
       <div class="flex -space-x-0.5" title="Weight">
         <dt class="sr-only">Weight</dt>
         <dd v-for="i in 5" :key="i">
-          <button
-            :class="[
-              item.weight >= i ? 'bg-primary-500' : 'bg-gray-800',
-              'h-6 w-6 rounded-full ring-2 ring-gray-900'
-            ]"
-            tabindex="-1"
-            @click="setItemWeight(i)"
-          ></button>
+          <button :class="[
+            item.weight >= i ? 'bg-primary-500' : 'bg-gray-800',
+            'h-6 w-6 rounded-full ring-2 ring-gray-900'
+          ]" tabindex="-1" @click="setItemWeight(i)"></button>
         </dd>
       </div>
       <div class="">
